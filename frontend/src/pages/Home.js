570abd24c5b314cfff79c4bfbb4f215c3fcf945a
Uttam Negi <piyush.negi@kalvium.community>
2023-07-02 11:57:22 +0530

    Navbar created

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+      Hello World
+    </div>
+  )
+}
+
+export default Home
