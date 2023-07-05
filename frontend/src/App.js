37079bfec746d80e446eeb8cf85ce07423f23c80
praduman03 <praduman03k@gmail.com>
2023-07-05 15:01:11 +0530

    Created Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css';
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
