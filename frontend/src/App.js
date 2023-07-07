9863ba9cae1db8e7fbf82b2ef8debf0182d93194
praduman03 <praduman03k@gmail.com>
2023-07-07 15:14:41 +0530

    Completed Contact Us page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Contactus from "./pages/Contactus";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contactus" element={<Contactus />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Help Part/Questions.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Help.css";
+import Waves from "../../assets/waves.svg";
+
+function Questions() {
+  return (
+    <div className="Question-container">
+      <div>
+        <h1>Have Some Questions?</h1>
+        <p>
+          Thank you for your interest in our services. Please fill out the form
+          below or email us and we will get <br /> back to you promptly
+          regarding your request.
+        </p>
+      </div>
+      <div>
+        <img src={Waves} />
+      </div>
+    </div>
+  );
+}
+
+export default Questions;
