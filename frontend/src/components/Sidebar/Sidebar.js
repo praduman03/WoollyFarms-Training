cea4d7dd5892ddeef566f0b0400fd01eb4cafa95
PiyushNegi1703 <negip7740@gmail.com>
2023-07-14 12:19:29 +0530

    Completed Sidebar

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/LOGO.jpg";
 import "./Sidebar.css";
 import { BsMegaphoneFill } from "react-icons/bs";
@@ -8,27 +8,56 @@ import { LuLogOut } from "react-icons/lu";
 import { CgProfile } from "react-icons/cg";
 
 const Sidebar = () => {
+  const [alert, setAlert] = useState(true);
+  const [event, setEvent] = useState(false);
+  const [group, setGroup] = useState(false);
+
   return (
     <div className="sidebar">
       <img src={Logo} alt="" className="side-logo" />
 
       <div className="nav-container">
         <ul>
-          <li>
+          <li
+            onClick={() => {
+              setAlert(!alert);
+              setEvent(false);
+              setGroup(false);
+            }}
+            className={alert ? 'alert' : 'side-links'}
+          >
             <BsMegaphoneFill /> Beware Neighbors
           </li>
-          <li>
+          <li
+            onClick={() => {
+              setAlert(false);
+              setEvent(!event);
+              setGroup(false);
+            }}
+            className={event ? 'event' : 'side-links'}
+          >
             <AiFillCalendar /> Welcome Neighbors
           </li>
-          <li>
+          <li
+            onClick={() => {
+              setAlert(false);
+              setEvent(false);
+              setGroup(!group);
+            }}
+            className={group ? 'group' : 'side-links'}
+          >
             <AiFillEye /> Watch Neighbors
           </li>
         </ul>
       </div>
 
       <div className="profile-section">
-        <div><CgProfile /> My Profile</div>
-        <div><LuLogOut /> Logout</div>
+        <div className="profile">
+          <CgProfile /> My Profile
+        </div>
+        <div className="logout">
+          <LuLogOut /> Logout
+        </div>
       </div>
     </div>
   );
