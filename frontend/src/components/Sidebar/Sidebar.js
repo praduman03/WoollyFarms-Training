19f878c21975aae940d7036a91b25e1986fae14d
PiyushNegi1703 <negip7740@gmail.com>
2023-07-14 10:22:12 +0530

    minor changes

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -4,6 +4,8 @@ import "./Sidebar.css";
 import { BsMegaphoneFill } from "react-icons/bs";
 import { AiFillCalendar } from "react-icons/ai";
 import { AiFillEye } from "react-icons/ai";
+import { LuLogOut } from "react-icons/lu";
+import { CgProfile } from "react-icons/cg";
 
 const Sidebar = () => {
   return (
@@ -24,7 +26,10 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div></div>
+      <div className="profile-section">
+        <div><CgProfile /> My Profile</div>
+        <div><LuLogOut /> Logout</div>
+      </div>
     </div>
   );
 };
